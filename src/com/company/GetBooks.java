a9c14cf7d059bb3b53d14d978de3f00722981f35
albert <malmkvist.albert@gmail.com>
2020-01-17 08:24:30 +0100

    Initial commit

---
+++ src/com/company/GetBooks.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GetBooks {
+private String list = "";
+
+    public GetBooks(){
+        this.list = list;
+    }
+}
