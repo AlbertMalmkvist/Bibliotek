9c3832d157c2419870a7a2ebd8d423c22ae2473c
albert <malmkvist.albert@gmail.com>
2020-01-20 16:22:43 +0100

    Change

--- src/com/company/Biblotek.java
+++ src/com/company/Biblotek.java
@@ -17,9 +17,11 @@ public class Biblotek {
             do {
 
                 System.out.println("Welcome to Sea Wars! Please select mode using key 1, 2 or 3. Press <ENTER>");
-                System.out.println("1. Player VS Player (PvP)");
-                System.out.println("2. Player VS Computer (PvE)");
-                System.out.println("3. Quit");
+                System.out.println("1. Borrow Book");
+                System.out.println("2. Return Book");
+                System.out.println("3. Add Book to Library(Librarians only)");
+                System.out.println("4. Remove Book from Library(Librarians only)");
+                System.out.println("5. Quit");
                 int userChoice = input.nextInt();
 
 
@@ -30,28 +32,32 @@ public class Biblotek {
                         System.out.println("You have chosen 'Player VS Player' (PvP)" + "\n");
 
                         //Welcoming players and let them enter their name
-                        System.out.println("Player 1, please type in your name");
-                        String playerOneName = inputFromUser.nextLine();
-                        System.out.println("Welcome," + " " + "!" + "\n");
-
-                        System.out.println("Player 2, please type in your name");
-                        String playerTwoName = inputFromUser.nextLine();
-                        System.out.println("Welcome," + " " + "!" + "\n");
+                        System.out.println("Please type in your username");
+                        String username1 = inputFromUser.nextLine();
+                        System.out.println("Welcome," + " " + username1  + "!" + "\n");
 
                         break;
 
                     case 2:
-                        System.out.println("You have chosen 'Player VS Computer' (PvE)" + "\n \n" + "Please enter your name:");
-
-                        String humanName = inputFromUser.nextLine();
-                        System.out.println("Welcome," + " " + "!");
-
-                        System.out.println("You'll be playing against the" + " "  + "\n");
-
+                        System.out.println("Please type in your username");
+                        String username2 = inputFromUser.nextLine();
+                        System.out.println("Welcome," + " " + username2  + "!" + "\n");
                         break;
 
 
                     case 3:
+                        System.out.println("Please enter password:");
+                        String username3 = inputFromUser.nextLine();
+                        System.out.println("Welcome, Librarian!\n");
+
+                        break;
+                    case 4:
+                        System.out.println("Please enter password:");
+                        String username4 = inputFromUser.nextLine();
+                        System.out.println("Welcome, Librarian!\n");
+
+                        break;
+                    case 5:
                         System.out.println("You will now Quit Sea Wars");
                         System.exit(0); // Shuts down program
                     default:
@@ -61,8 +67,7 @@ public class Biblotek {
             } while (gameStillRunning = true);
         } catch (InputMismatchException e) {
             e.getMessage();
-            System.out.println("Invalid choice. Please select 1, 2 or 3.");
-            //BattleShipProgram bA = new BattleShipProgram();
+            System.out.println("Invalid choice.");
         }
     }
 }
